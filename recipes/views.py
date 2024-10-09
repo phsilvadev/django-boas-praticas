8ff7a0da2accfabd48225635c21c6840ac41b639
phsilvadev <pedro6489@gmail.com>
2024-10-08 22:32:36 -0300

    removing time from recipes within templates

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request, 'recipes/home.html')
 
 def sobre(request):
-    return render(request, 'temp/temp.html')
+    return render(request, 'recipes/contato.html')
 
 def contatos(request):
     return HttpResponse('Contatos')
